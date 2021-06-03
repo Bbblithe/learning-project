6d553e01ba043f4eec128c78a22f413378b48e0e
blithe <2697891299@qq.com>
2021-06-03 19:45:35 +0800

    update code
    the learning of List struct

---
+++ Java-arithmetic/第五章/单链表/Link.java
@@ -0,0 +1,23 @@
+package 第五章.单链表;
+
+/**
+ * Author:  blithe.xwj
+ * Date:    2021/5/31 22:48
+ * Description:
+ */
+
+
+class Link {
+   public int iData;
+   public double dData;
+   public Link next;
+
+   public Link(int id,double dd){
+      iData = id;
+      dData = dd;
+   }
+
+   public void displayLink(){
+      System.out.print("{" + iData + ", " + dData + "} ");
+   }
+}
---
+++ Java-arithmetic/第五章/单链表/LinkList.java
@@ -0,0 +1,42 @@
+package 第五章.单链表;
+
+/**
+ * Author:  blithe.xwj
+ * Date:    2021/6/2 22:56
+ * Description:
+ */
+
+
+class LinkList {
+   private Link first;
+
+   public void LinkList(){
+      first = null;
+   }
+
+   public boolean isEmpty(){
+      return (first == null);
+   }
+
+   public void insertFirst(int id, double dd){
+      Link newLink = new Link(id,dd);
+      newLink.next = first;
+      first = newLink;
+   }
+
+   public Link deleteFirst(){
+      Link temp = first ;
+      first = first.next;
+      return temp;
+   }
+
+   public void displayList(){
+      System.out.print("List (first-->last): ");
+      Link current = first;
+      while(current != null ){
+         current.displayLink();
+         current = current.next;
+      }
+      System.out.println("");
+   }
+}
